29a859757f3820593698b0f0a7bf2102536daac6
Adrian Devecz <adrian.devecz@flex.com>
2021-10-18 01:37:44 +0200

    Checkbox bugfix

--- src/atoms/Checkbox.jsx
+++ src/atoms/Checkbox.jsx
@@ -4,7 +4,7 @@ import { FiCheckSquare, FiSquare } from 'react-icons/fi';
 export const Checkbox = ({ checked, value, onChange = value => { }, children, ...props }) => {
     const propsBase = {
         ...props, tabIndex: 0,
-        onClick: () => onChange(value || !checked)
+        onClick: () => onChange(value ?? !checked)
     };
     return <label {...propsBase}>{checked ? <FiCheckSquare /> : <FiSquare />}{children}</label>
 };
