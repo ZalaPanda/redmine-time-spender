b28a29d0affe0dd626d807e2545071fb2c6658f2
Adrian Devecz <adrian.devecz@flex.com>
2021-11-20 01:02:09 +0100

    Collapsible height fix on height reduce

--- src/atoms/Collapsible.jsx
+++ src/atoms/Collapsible.jsx
@@ -5,6 +5,7 @@ export const Collapsible = ({ open, children, ...props }) => {
     const ref = useRef();
     const [{ height }, setSpring] = useSpring(() => ({ height: 0, immediate: true }));
     useEffect(() => {
+        if (open) ref.current.style.height = 'auto'; // QUESTION: only animate on open change?
         const height = open ? ref.current.scrollHeight : 0;
         setSpring.start({ height });
     }, [children, open]);
