0441a93bfaa1fb6b50a6355f4ad55c77e21aa774
Adrian Devecz <adrian.devecz@fuba-automotive.com>
2022-12-22 12:26:05 +0100

    more typescript for Textarea

--- src/atoms/Textarea.tsx
+++ src/atoms/Textarea.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, HTMLAttributes } from 'react';
 
-export const Textarea = (props) => {
+export const Textarea = (props: { value: string } & HTMLAttributes<HTMLTextAreaElement>) => {
     const element = useRef<HTMLTextAreaElement>();
     useEffect(() => {
         element.current.style.height = '0px'; // reset height
