6535d28150acb76a71497c89d9af19d697eb51d8
Adrian Devecz <adrian.devecz@fuba-automotive.com>
2023-01-01 23:22:04 +0100

    removed console log from Textarea

--- src/atoms/Textarea.tsx
+++ src/atoms/Textarea.tsx
@@ -3,7 +3,6 @@ import { useEffect, useRef, HTMLAttributes } from 'react';
 export const Textarea = ({ defaultValue, ...props }: HTMLAttributes<HTMLTextAreaElement>) => {
     const element = useRef<HTMLTextAreaElement>();
     const adjustSize = () => {
-        console.log(element.current.value, element.current.style.height);
         element.current.style.height = '0px'; // reset height
         element.current.style.height = element.current.scrollHeight && `${Math.min(element.current.scrollHeight, 85)}px` || ''; // set scroll height - max 3 rows
     };
